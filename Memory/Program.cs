4d4caf192c69a375e3638f72ff3d69951a8d5a0d
NadavAharoni <65665296+NadavAharoni@users.noreply.github.com>
2024-03-29 16:01:45 +0300

    Initial ExpandArray function

--- Memory/Program.cs
+++ Memory/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] a = { 1, 2, 3 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            ExpandArray(a);
+
+            Console.WriteLine();
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+
+        static void ExpandArray(int[] array)
+        {
+            int[] oldArray = array;
+            int arrayLength = array.Length;
+            array = new int[arrayLength * 2];
+            for (int i = 0; i < arrayLength; i++)
+            {
+                array[i] = oldArray[i];
+                array[i * 2] = oldArray[i];
+            }
         }
     }
 }
